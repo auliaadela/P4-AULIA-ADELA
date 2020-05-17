88b042172948588421f495bf52d205ff50eff5d6
aulia adela <65493429+auliaadela@users.noreply.github.com>
2020-05-17 20:40:24 +0700

    Create jobsheet 8 C++.cpp
    Buatlah program mengisi nilai array dan menampilka nnnyadengan indeks array dan batas perulangan menggunakan nim terakhir setiap mahasiswa

---
+++ jobsheet 8 C++.cpp
@@ -0,0 +1,21 @@
+#include <iostream> using namespace std; int main()
+{	//akhiran nim 0
+int x[5],i; for(i=0;i<5;i++)
+{
+
+cout<<"masukan	nilai "<<i+1<<" : ";
+
+cin>>x[i];
+
+}
+
+cout<<endl; for(i=0;i<0;i++)
+{
+
+cout<<"nilai	ke	"<<i+1<<"	= "<<x[i]<<endl;
+
+}
+
+return 0;
+
+}
