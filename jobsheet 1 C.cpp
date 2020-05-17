31207725955faac2bcb3c2c50910fe15c0093ac2
aulia adela <65493429+auliaadela@users.noreply.github.com>
2020-05-17 20:06:24 +0700

    Create jobsheet 1 C.cpp
    Buatlah program array satu dimensi yang menampilkan NIM masing masing

---
+++ jobsheet 1 C.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+main()
+{
+char* nim_ray[5]={"f1b019030","f1b019014","f1b019018","f 1b019020","f1b019022",};
+printf("nim mahasis wa ke- 1 =
+%s\n",nim_ray[0]);
+printf("nimmahasiswa ke- 2 =
+%s\n",nim_ray[1]);
+printf("nimmahasiswa ke- 3 =
+%s\n",nim_ray[2]);
+printf("nimmahasiswa ke- 4 =
+%s\n",nim_ray[3]);
+printf("nimmahasiswa ke- 5 =
+%s\n",nim_ray[4]);
+printf("nimmahasiswa ke- 6 =
+%s\n",nim_ray[5]);
+}
