349d278d9b9583cefed99ef3b55841b9ca05d4b6
aulia adela <65493429+auliaadela@users.noreply.github.com>
2020-05-17 20:11:04 +0700

    Create jobsheet 1 C++.cpp
    Buatlah program array satu dimensi yang menampilkan NIM masing masing

---
+++ jobsheet 1 C++.cpp
@@ -0,0 +1,12 @@
+#include <iostream> using namespace std; int main()
+{
+cout<<"Aulia Adela- F1B019030\n\n";
+int nim[5]= {30,0,9,2,3};
+
+cout << "Nim ke-1 adalah " << nim[0] << endl;
+cout << "Nim ke-2 adalah " << nim[1] << endl;
+cout << "Nim ke-3 adalah " << nim[2] << endl;
+cout << "Nim ke-4 adalah " << nim[3] << endl;
+cout << "Nim ke-5 adalah " << nim[4] << endl;
+
+}
