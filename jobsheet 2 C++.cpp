bde7274c8137af0ec6a40a53426d8da1dd49f062
aulia adela <65493429+auliaadela@users.noreply.github.com>
2020-05-17 20:20:28 +0700

    Create jobsheet 2 C++.cpp
    Buatlah program mengisi dan menampilkan elemen array dengan index array sesuai dengan NIM terakhir setiap mahasiswa

---
+++ jobsheet 2 C++.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std; int main(){
+// Deklarasi sekaligus inisialisasi (pengisian awal)
+// Array 'ARnilai' dengan 5 buah elemen berisi int
+int ARnilai[5]={30,4,5,10,2};
+	
+// Karena sudah melakukan inisialisasi maka kita
+// Tidak wajib mengisi nilai kedalam Elemen Array
+
+//menampilkan nilai atau isi dari array 'ARnilai'
+for(int i=0; i<5;i++){ cout<<"Tampil nilai indek
+ke ["<<i<<"] = "<<ARnilai[i];
+cout<<endl;
+}
+return 0;
+}
